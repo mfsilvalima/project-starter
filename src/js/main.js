0ed2cfdd6aceda3f9eba563ed7c0e8ad155ad11b
Mateus Fernandes <mf.silvalima@gmail.com>
2016-03-28 17:39:23 -0300

    js function to insert animation with scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// you can start
+function animacoes(){
+  "use strict";
+  $('.animated').each(function(){
+    var posItem = $(this).offset().top + 100;
+
+    if(posItem > $(window).scrollTop() && posItem < $(window).scrollTop() + $(window).height()){
+
+      $(this).addClass('visible');
+      $(this).addClass($(this).data("animation"));
+    }
+  });
+}
+
+$(document).on('ready', function () {
+    $(window).on('scroll', function(){
+      animacoes();
+    });
+});
